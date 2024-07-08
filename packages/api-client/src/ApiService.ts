1486430cc234473a76ff69393160b9a3b3cb66da
Mia Pilchová <valisova@lilianaa.dev>
2024-07-08 09:36:43 +0200

    api-client: fix compatibility with v1 api errors

--- packages/api-client/src/ApiService.ts
+++ packages/api-client/src/ApiService.ts
@@ -26,21 +26,35 @@ export class ApiService {
   }
 
   public handleException(e: any): ApiErrorResponse {
-    return {
+    const result: ApiErrorResponse = {
       success: false,
-      statusCode: e?.response?.data?.statusCode ?? e?.response?.statusCode ?? e?.statusCode ?? -1,
+      statusCode: e?.response?.data?.status ?? e?.response?.status ?? e?.status ?? -1,
       error: {
         type: e?.response?.data?.error?.type ?? e?.error?.type ?? ApiError.UnknownError,
         message: this.getErrorMessage(e),
       },
       meta: e?.response?.data?.meta ?? e?.meta ?? {},
     };
+
+    console.log("handleException", { input: e, result })
+
+    return result;
   }
 
+  // fuck me, what kind of state was I in to write this shit
   public getErrorMessage(e: any | ApiError | AxiosError | ApiErrorResponse): string {
+    // only accept an object or a string
     if (!e || !['object', 'string'].includes(typeof e)) return ApiErrorMessage[ApiError.UnknownError];
-    if (typeof e === 'object')
+
+    if (typeof e === 'object') {
+      // api v1 error compatibility
+      if (typeof e?.response?.data?.error === "string") {
+        return e.response.data.error;
+      }
+
+      // find error message by it's type
       e = apiError(e?.response?.data?.error?.type ?? e?.error?.type ?? e?.type);
+    }
 
     if (e in ApiErrorMessage) {
       return (ApiErrorMessage as any)[e];
